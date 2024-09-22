56acf726e66e398f7374378a5690e56a79689c88
yhanege <yavuz.hanege@delivers.ai>
2024-09-22 21:17:03 +0300

    [+] 13.09.2024 Lesson: Arrays and Basic Algorithms

--- Notes/C Programming Notes - Necati Ergin.md
+++ Notes/C Programming Notes - Necati Ergin.md
@@ -3675,3 +3675,52 @@ int a[10] = {[4] = 10, [2] = 20, [5] = 40};
 ---
 # 13.09.2024
 
+## Odev 1
+
+```c
+
+// ODEV 1 
+// Dizi içerisindeki unique elemanları bulunuz.
+#if 1
+
+#define SIZE 20
+
+int main(void){
+
+	int a[SIZE];
+	int cnt = 0;
+	for(int i = 0; i < SIZE; ++i){
+
+		a[i] = rand() % 20; 
+		printf("%d ",a[i]); 
+	}
+	printf("\n");
+	for(int i = 0; i < SIZE; ++i){
+	    cnt = 0;
+		for(int j = 0; j < SIZE; ++j){
+			if(a[i] == a[j]){
+				//printf("%d ile %d esittir\n",i,j);
+				cnt++;
+			}
+			
+		}
+		if(cnt == 1){
+		printf("unique = %d \n", a[i]);
+
+		}		
+	}
+	printf("cnt = %d\n",cnt);
+
+}
+
+#endif
+
+```
+
+> [!NOTE] Bu derste fazlasıyla örnek çözüldüğü için not tutmak yerine `main.c` dosyasında bulunan kodları kendim yazdım.
+
+## sizeof Operator
+
+
+
+
--- libs/Src/main.c
+++ libs/Src/main.c
@@ -94,19 +94,172 @@ int main(void){
 
 #endif
 
-#if 1
+#if 0
 
-#define SIZE 100
+#define SIZE 20
 
 int main(void){
 	
 	int a[SIZE];
 	
-	// randomize();
-	//  set_array_random(a,SIZE);
-	//  print_array(a,SIZE);
-	printf("sakjdkjasb\n");
+	randomize();
+	set_array_random(a,SIZE);
+	print_array(a,SIZE);
+
+	int sum = 0;
+	for(int i = 0; i < SIZE; ++i){
+		sum += a[i];
+		
+	}
+	printf("sum = %d\n",sum); // toplamını bulma formülü
+	
+	printf("mean = %d\n",(double)sum / SIZE); // ortalamasını bulma formülü
+
+	int max = a[0];
+	int min = a[0];
+
+	for(int i = 1; i < SIZE; ++i){
+
+		max = a[i] > max ? a[i] : max ; // max 
+		min = a[i] < min ? a[i] : min ; // min
+	}
+	printf("maximum = %d\n",max);
+	printf("minimum = %d\n",min);
+
+
 }
 
+#endif
+
+// ODEV 1
+// Dizi içerisindeki unique elemanları bulunuz.
+#if 0
+
+#define SIZE 20
+
+int main(void)
+{
+	int a[SIZE];
+	int flag = 0;
+	for (int i = 0; i < SIZE; ++i)
+	{
+
+		a[i] = rand() % 20;
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+	for (int i = 0; i < SIZE; ++i)
+	{
+		flag = 0;
+		for (int j = 0; j < SIZE; ++j)
+		{
+			if (a[i] == a[j])
+			{
+				// printf("%d ile %d esittir\n",i,j);
+				flag++;
+			}
+		}
+		if (flag == 1)
+		{
+			printf("unique = %d", a[i]);
+		}
+	}
+}
+
+#endif
+
+
+// LINEAR SEARCH ALGORITHM
+#if 0
+
+#define SIZE 20
+
+int main(void){
+	int a[SIZE];
+	randomize();
+	set_array_random(a,SIZE);
+	print_array(a,SIZE);
+	int key;
+	int i;
+
+	printf("Aranacak degeri giriniz: ");
+	scanf("%d",&key);
+	for(i = 0; i < SIZE && a[i] != key; ++i){
+		; // null statement
+	}
+
+	if(i < SIZE){
+		printf("bulundu idx = %d\n",i);
+	}
+	else{
+		printf("bulunamadi\n");
+
+
+	}
+
+}
+
+#endif
+
+#if 0
+// Fonksiyon her çağırıldığında 0-urand_max arasında rastgele sayı üretmeli.
+// bu durumda fonksiyon en fazla URAND_MAX kadar sayi üretebilir.
+// Daha fazla çağırılırsa unique random sayi kalmamışsa hata olarka -1 döndürmeli.
+#define URAND_MAX      20
+
+int urand(void){
+
+	static int flags[URAND_MAX] = { 0 };
+	static int cnt = 0;
+
+	if(cnt == URAND_MAX)
+		return -1;
+
+
+	int val;
+	for(; ;){ // while(1) ile aynı şey
+	val = rand() % URAND_MAX;
+		if(flags[val]  == 0){
+			break;
+		}
+
+	}
+
+	flags[val] = 1;
+	++cnt;
+
+	return val;
+}
+
+int main(void){
+	randomize();
+	for(int i = 0; i < URAND_MAX; ++i){
+
+		printf("%d ",urand());
+	}
+	printf("\n\n%d\n ",urand());
+
+
+}
+
+#endif
+
+
+// Diziyi ters çevirme algoritması
+#if  0
+
+#define SIZE 20
+
+int main(void){
+	int a[SIZE];
+	randomize();
+	set_array_random(a,SIZE);
+	print_array(a,SIZE);
+	for(int i = 0; i < SIZE/2 ; ++i){
+		int temp = a[i];
+		a[i] = a[SIZE-i-1];
+		a[SIZE-i-1] = temp;	}
+	print_array(a,SIZE);
+}
 #endif
 
